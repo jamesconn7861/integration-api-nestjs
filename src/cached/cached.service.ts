96c0d193954c91419f6fabd6baeea40abaacb7a1
PMRYjconn <103073234+PMRYjconn@users.noreply.github.com>
2023-01-13 09:49:29 -0500

    Created types for all cached data

--- src/cached/cached.service.ts
+++ src/cached/cached.service.ts
@@ -1,40 +1,91 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { DbService } from '../db/db.service';
 import * as fs from 'fs';
+import {
+  SwitchObject,
+  TableObject,
+  TableProperites,
+  VlanObject,
+} from './types';
 
 @Injectable()
-export class CachedService implements OnModuleInit {
-  vlanSchema: any;
-  switchSchema: any;
-  tableSchema: any;
-  constructor(private db: DbService) {}
-
-  onModuleInit() {
+export class CachedService {
+  constructor(private db: DbService) {
     this.updateCachedData();
   }
 
+  vlanSchema: VlanObject[] = [];
+  switchSchema: SwitchObject[] = [];
+  tableSchema: TableObject[] = [];
+
   async updateCachedData() {
-    this.tableSchema = JSON.parse(
+    this.vlanSchema = [];
+    this.switchSchema = [];
+    this.tableSchema = [];
+
+    let tableSchemaData = JSON.parse(
       fs.readFileSync(__dirname + '../../../tableSchema.json').toString(),
     );
 
+    tableSchemaData['tables'].forEach((table: any) => {
+      table['columns'] = table['columns'] as TableProperites;
+      this.tableSchema.push(table as TableObject);
+    });
+
     const conn = await this.db.pool.promise().getConnection();
 
-    let test = 5;
-    let [rows, fields] = await conn.query('Select * from `vlans`');
-    this.vlanSchema = rows;
+    let [rows, _fields] = await conn.query('Select * from `vlans`');
 
-    [rows, fields] = await conn.query('Select * from `benches`');
-    this.switchSchema = rows;
+    rows.forEach((row: Partial<VlanObject>) => {
+      this.vlanSchema.push(row as VlanObject);
+    });
+
+    [rows, _fields] = await conn.query('Select * from `benches`');
+
+    rows.forEach((row: Partial<SwitchObject>) => {
+      this.switchSchema.push(row as SwitchObject);
+    });
 
     this.db.pool.releaseConnection(conn);
   }
 
-  async getAppInitData() {
+  getAppInitData() {
     return {
       tableSchema: this.tableSchema,
       vlanSchema: this.vlanSchema,
       switchSchema: this.switchSchema,
     };
+  }
+
+  getBenchSchema(): SwitchObject[] {
+    return this.switchSchema;
+  }
+
+  getBenchSchemaById(benchId: string | number): SwitchObject {
+    if (typeof benchId == 'string') {
+      return this.switchSchema.find(
+        (bench) => bench.id.toLowerCase() === benchId.toString().toLowerCase(),
+      );
+    } else {
+      return this.switchSchema.find((bench) => bench.switch === benchId);
+    }
+  }
+
+  getVlanSchema(): VlanObject[] {
+    return this.vlanSchema;
+  }
+
+  getVlanSchemaById(vlanId: string | number): VlanObject {
+    if (typeof vlanId == 'string') {
+      return this.vlanSchema.find((vlan) =>
+        vlan.name.toLowerCase().includes(vlanId.toLowerCase()),
+      );
+    } else {
+      return this.vlanSchema.find((vlan) => vlan.id === vlanId);
+    }
+  }
+
+  getCachedData(dataKey: string) {
+    return this[dataKey];
   }
 }
