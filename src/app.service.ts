042a5d10d3ba4ed6e42cad6419f26f5e57981b77
PMRYjconn <103073234+PMRYjconn@users.noreply.github.com>
2023-01-16 12:30:54 -0500

    Added comments and TODOs

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,3 +2,9 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {}
+
+// TODO Delete this class if not used before production.
+/*
+Currently don't have any plans for root app services. 
+Everything is handled by their respective modules.
+*/
