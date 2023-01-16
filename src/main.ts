6a8093830af9f998304a202eda0ff23fe8079589
PMRYjconn <103073234+PMRYjconn@users.noreply.github.com>
2023-01-16 12:31:29 -0500

    Added global pipes and comments on fastify

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,28 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
+  /*
+    NOTE Changed from express to fastify. Dramatically increased response times.
+    As of now, all functions work exactly the same as if express was used. 
+    Reverting back to express shouldn't* break any code or at least very little.
+  */
+
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter({ logger: true }),
   );
 
+  // Added whitelisting to remove unwanted body keys.
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+
+  // TODO Change this to the correct port before production build
   await app.listen(3000);
 }
 bootstrap();
