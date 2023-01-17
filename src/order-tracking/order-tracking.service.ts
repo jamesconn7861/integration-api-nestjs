05eed064eb5380f052183c9e96344ec55d8031dc
PMRYjconn <103073234+PMRYjconn@users.noreply.github.com>
2023-01-17 14:09:08 -0500

    changed param type for getOrdersByUser

--- src/order-tracking/order-tracking.service.ts
+++ src/order-tracking/order-tracking.service.ts
@@ -1,18 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { DbService } from '../db/db.service';
 import { EditOrderDto, NewOrderDto } from './dtos';
+import { OrderArrayDto } from './dtos/order-array.dto';
 
 @Injectable()
 export class OrderTrackingService {
   constructor(private db: DbService) {}
 
-  async getOrdersByUser(userId: string) {
+  async getOrdersByUser(userId: string, isActive: string) {
     let queryString: string;
 
     if (userId === 'all') {
-      queryString = `select * from integrationdb.order_tracking`;
+      queryString = `select * from integrationdb.order_tracking where is_active in (${isActive}) Limit 500`;
     } else {
-      queryString = `select * from integrationdb.order_tracking where tech = '${userId}'`;
+      queryString = `select * from integrationdb.order_tracking where tech = '${userId}' and is_active in (${isActive}) Limit 500`;
     }
 
     const [rows, _fields] = await this.db.pool.promise().query(queryString);
@@ -28,16 +29,27 @@ export class OrderTrackingService {
     return [rows];
   }
 
-  async uploadOrders(orderDetails: NewOrderDto[]) {
+  async uploadOrders(orderDetails: NewOrderDto) {
     let queryString: string;
+    let queryArray: any[][] = [];
 
-    if (orderDetails.length > 1) {
+    orderDetails.orderNumbers.forEach((orderNumber: number) => {
+      queryArray.push([
+        orderNumber,
+        orderDetails.tech,
+        orderDetails.startTime,
+        orderDetails.isActive,
+        orderDetails.hasIssue,
+      ]);
+    });
+
+    if (orderDetails.orderNumbers.length > 1) {
       queryString = `insert ignore into integrationdb.order_tracking (order_number, tech, start_time, is_active, has_issue) values ?`;
     } else {
       queryString = `insert into integrationdb.order_tracking (order_number, tech, start_time, is_active, has_issue) values ?`;
     }
 
-    return await this.db.pool.promise().query(queryString, [orderDetails]);
+    return await this.db.pool.promise().query(queryString, [queryArray]);
   }
 
   async updateOrder(updateDetails: EditOrderDto) {}
