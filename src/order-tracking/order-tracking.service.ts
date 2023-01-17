d926f64e64c82c79344a4d9a7b4d411a21dbd2bb
PMRYjconn <103073234+PMRYjconn@users.noreply.github.com>
2023-01-17 09:30:50 -0500

    Updated module path

--- src/order-tracking/order-tracking.service.ts
+++ src/order-tracking/order-tracking.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { DbService } from 'src/db/db.service';
+import { DbService } from '../db/db.service';
 import { EditOrderDto, NewOrderDto } from './dtos';
 
 @Injectable()
