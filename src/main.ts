5d84907688a186821b125ddaa5bf4c83d5b24062
PMRYjconn <103073234+PMRYjconn@users.noreply.github.com>
2023-01-17 14:07:40 -0500

    Added enableImplicitConversion: true

--- src/main.ts
+++ src/main.ts
@@ -22,6 +22,9 @@ async function bootstrap() {
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
     }),
   );
 
