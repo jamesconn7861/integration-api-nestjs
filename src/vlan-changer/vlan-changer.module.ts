32dfdf95692beba70dc143dbccd1702e28cf5a92
PMRYjconn <103073234+PMRYjconn@users.noreply.github.com>
2023-01-19 09:02:28 -0500

    Removed middleware. Now handled by hook

--- src/vlan-changer/vlan-changer.module.ts
+++ src/vlan-changer/vlan-changer.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { MiddlewareConsumer } from '@nestjs/common/interfaces';
-import { bodyLogger } from 'src/middleware/logger/logger';
 import { CachedModule } from '../cached/cached.module';
 import { SshModule } from '../ssh/ssh.module';
 import { VlanChangerController } from './vlan-changer.controller';
@@ -11,8 +9,4 @@ import { VlanChangerService } from './vlan-changer.service';
   controllers: [VlanChangerController],
   providers: [VlanChangerService],
 })
-export class VlanChangerModule {
-  // configure(consumer: MiddlewareConsumer) {
-  //   consumer.apply(bodyLogger).forRoutes('vlan-changer/change');
-  // }
-}
+export class VlanChangerModule {}
