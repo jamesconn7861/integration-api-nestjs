6afdb92d1579e3c29efdb095923fa193c9829b42
PMRYjconn <103073234+PMRYjconn@users.noreply.github.com>
2023-01-20 08:49:19 -0500

    Changed path for debug.log

--- src/main.ts
+++ src/main.ts
@@ -25,9 +25,12 @@ async function bootstrap() {
     vlan: 13,
   };
 
+  let env = process.env.NODE_ENV || 'dev';
+  let logPath: string = env == 'dev' ? `./logs/api.log` : `./logs/debug.log`;
+
   const fastifyInstance = fastify({
     logger: {
-      file: `./logs/api.log`,
+      file: logPath,
       customLevels: levels,
       useOnlyCustomLevels: true,
     },
