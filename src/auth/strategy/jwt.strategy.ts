4981e8d59423b374c9be0284de258c13880707a0
PMRYjconn <103073234+PMRYjconn@users.noreply.github.com>
2023-01-27 10:11:08 -0500

    changed from prisma to mysql

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -1,38 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
-import {
-  ExtractJwt,
-  Strategy,
-} from 'passport-jwt';
-import { PrismaService } from '../../prisma/prisma.service';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { DbService } from '../../db/db.service';
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(
-  Strategy,
-  'jwt',
-) {
-  constructor(
-    config: ConfigService,
-    private prisma: PrismaService,
-  ) {
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(config: ConfigService, private dbService: DbService) {
     super({
-      jwtFromRequest:
-        ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: config.get('JWT_SECRET'),
     });
   }
 
-  async validate(payload: {
-    sub: number;
-    email: string;
-  }) {
-    const user =
-      await this.prisma.user.findUnique({
-        where: {
-          id: payload.sub,
-        },
-      });
+  async validate(payload: { id: number; email: string }) {
+    // const user =
+    //   await this.prisma.user.findUnique({
+    //     where: {
+    //       id: payload.sub,
+    //     },
+    //   });
+    const user = await this.dbService.pool
+      .promise()
+      .query('select * from users where id = ?', [payload.id]);
     delete user.hash;
     return user;
   }
