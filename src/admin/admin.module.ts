7d41bbe6a65f0167a3010263a096fd228d55094c
PMRYjconn <james.conn@pomeroy.com>
2023-02-17 16:20:05 -0500

    Added CachedModule to imports

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { CachedModule } from 'src/cached/cached.module';
 import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
 
 @Module({
   controllers: [AdminController],
-  providers: [AdminService]
+  providers: [AdminService],
+  imports: [CachedModule],
 })
 export class AdminModule {}
