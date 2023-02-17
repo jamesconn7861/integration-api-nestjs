cff970f4529a22db508ee1a5eab88ba733629f2f
PMRYjconn <james.conn@pomeroy.com>
2023-02-17 16:20:28 -0500

    renamed file

--- src/admin/dtos/visibilityEnums.ts
+++
@@ -1,5 +0,0 @@
-export enum VisibilityEnum {
-  Visible = 1,
-  Hidden = 2,
-  Admin = 3,
-}
---
+++ src/admin/dtos/vlanEnums.ts
@@ -0,0 +1,19 @@
+enum VisibilityEnum {
+  Visible = 1,
+  Hidden = 2,
+  Admin = 3,
+}
+
+enum DepartmentEnum {
+  Integration = 1,
+  Depot = 2,
+  'Asset Disposistion' = 3,
+  Universal = 4,
+}
+
+enum ProtectionEnum {
+  Unprotected,
+  Protected,
+}
+
+export { VisibilityEnum, DepartmentEnum, ProtectionEnum };
